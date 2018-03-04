f8b5a0920ca70f73e59ac3717caecf1a534ee189
Dave Smythe <dlsmythe@gmail.com>
2018-03-04 11:52:06 -0600

    Tune-up of 13 lisp.

--- adv17-13.lisp
+++ adv17-13.lisp
@@ -7,6 +7,13 @@
 ;;  This is a translation of the second attempt at solving this one, in C.
 ;;  There is still much room for improvement.  For instance, instead of representing
 ;;  the state of the firewall as a list of layers, a vector would be much better.
+;;
+;; New here:
+;; - maphash
+;; - mapcar
+;; - find-if
+;; - explicitly setting pointers to nil to avoid blowing the heap (see prune-simlist)
+;; - assert
 
 (proclaim '(optimize (speed 3) (safety 0)))
 ;;(proclaim '(optimize (debug 3) (speed 0) (safety 3)))
@@ -14,74 +21,10 @@
 (ql:quickload "getopt" :silent t)
 (ql:quickload "cl-ppcre" :silent t)
 
+(load "queues.lisp")
+
 (defparameter *verbose* 0)
 (defparameter *max-depth* 0) ;; length of *node-heap* below should be this squared
-
-;; First, a few generic helper routines
-
-(defun join (separator list)
-  (with-output-to-string (out)
-    (loop for (element . more) on list
-       do (princ element out)
-       when more
-       do (princ separator out))))
-
-(defun copy-table (table)
-  (let ((new-table (make-hash-table
-                    :test (hash-table-test table)
-                    :size (hash-table-size table))))
-    (maphash #'(lambda(key value)
-                 (setf (gethash key new-table) value))
-             table)
-    new-table))
-
-;;===========
-
-(defclass queue ()
-  ((list :initform nil)
-   (tail :initform nil)))
-
-(defmethod print-object ((queue queue) stream)
-  (print-unreadable-object (queue stream :type t)
-    (with-slots (list tail) queue
-      (cond ((cddddr list)
-	     ;; at least five elements, so print ellipsis
-	     (format stream "(~{~S ~}... ~S)"
-		     (subseq list 0 3) (first tail)))
-	    ;; otherwise print whole list
-	    (t (format stream "~:S" list))))))
-
-(defgeneric dequeue (queue)
-  (:documentation "Remove the next item from the front of the queue."))
-(defmethod dequeue ((queue queue))
-  (with-slots (list) queue
-    (pop list)))
-
-(defgeneric enqueue (queue item)
-  (:documentation "Add an item to the end of the queue."))
-(defmethod enqueue ((queue queue) new-item)
-  (with-slots (list tail) queue
-    (let ((new-tail (list new-item)))
-      (cond ((null list) (setf list new-tail))
-	    (t (setf (cdr tail) new-tail)))
-      (setf tail new-tail)))
-  queue)
-
-(defgeneric contains (queue item)
-  (:documentation "Return whether an item is in the queue."))
-(defmethod contains ((queue queue) item)
-  (with-slots (list) queue
-      (find item list)))
-
-(defgeneric empty (queue)
-  (:documentation "Return whether the queue is empty."))
-(defmethod empty ((queue queue))
-  (with-slots (list) queue
-      (null list)))
-
-(defgeneric members (queue))
-(defmethod members ((queue queue))
-  (slot-value queue 'list))
 
 ;;=========================================================
 
@@ -93,7 +36,11 @@
    (scanpos :initarg :scanpos :initform 0 :accessor layer-scanpos)
    (increment :initarg :increment :initform 1 :accessor layer-increment)))
 
-;; This is the initial description of the firewall, read from the user.
+(defmethod print-object ((layer layer) stream)
+  (with-slots (index depth range scanpos increment) layer
+    (format stream "#<layer: index ~a depth ~a range ~a scanpos ~a increment ~a>" index depth range scanpos increment)))
+
+;; This is the initial description of the firewall, read from the user. It is a layer list.
 (defparameter *input-list* nil)
 
 ;; This is a cache of class 'layer' objects, to avoid constant malloc/free
@@ -133,42 +80,27 @@
   (mapcar #'layer-free l))
 
 (defun copy-layer-list (l)
+  (assert (not (null l)) (l))
+  (assert (typep l 'cons) (l))
+  (assert (typep (car l) 'layer) (l))
   (loop for n in l collect (layer-alloc (layer-depth n) (layer-range n) (layer-scanpos n) (layer-increment n))))
 
 (defun find-layer-with-depth (l depth)
   (find-if (lambda (n) (= depth (layer-depth n))) l))
 
-;;===================================================================
-
-;; Keep the last 100 picoseconds of data so we don't have to keep regenerating it
-(defparameter *max-to-keep* 100)
-(defparameter *max-kept* 0)
-
-(defclass fw-state ()
-  ((simtime :initarg :simtime :initform 0 :accessor fw-state-simtime)
-   (layers :initarg :layers :initform nil :accessor fw-state-layers)))
-
-;; *simtimelist* is a queue containing elements of type fw-state
-(defparameter *simtimelist* (make-instance 'queue))
-
-(defun prune-simlist ()
-  (do ()
-      ((<= *max-kept* *max-to-keep*))
-    (let ((s (dequeue *simtimelist*)))
-      (if (> *verbose* 1)
-	  (format t "Deleted state at time ~d~%" (fw-state-simtime s)))
-      (free-layer-list (fw-state-layers s))
-      (setf (fw-state-layers s) nil) ;; hopefully avoid some garbage
-      (decf *max-kept*))))
-
-(defun add-fw-state (s)
-  (if (> *verbose* 1)
-      (format t "Added state at time ~d~%" (fw-state-simtime s)))
-  (enqueue *simtimelist* s)
-  (incf *max-kept*)
-  (prune-simlist))
-
-;; ====================
+;; Update the scan position for this layer
+(defgeneric layer-advance (layer))
+(defmethod layer-advance ((l layer))
+  (with-slots (increment scanpos range) l
+    (let ((newpos (+ scanpos increment)))
+      (cond ((>= newpos range)
+	     (decf newpos 2)
+	     (setf increment (- increment)))
+	    ((= -1 newpos)
+	     (setf newpos 1)
+	     (setf increment (- increment))))
+      (setf scanpos newpos)))
+ l)
 
 ;; returns a list of layer nodes
 (defun read-input ()
@@ -187,36 +119,80 @@
 		     (setf *max-depth* depth))
 		 (layer-alloc depth rng 0 1)))))
 
-(defun prt-state (states pos)
-  (dotimes (i (1+ *max-depth*))
-    (let ((n (find-layer-with-depth states i)))
-      (if (null n)
-	  (format t "~3d ~[...~;(.)~]~%" i (if (= i pos) 1 0))
-	  (let ((scanpos (layer-scanpos n))
-		(r (layer-range n)))
-	    (format t "~3d ~a~%" i 
-		    (with-output-to-string (layer)
-		      (dotimes (j r)
-			(let ((print-parens (if (and (= 0 j) (= i pos)) 0 1)))
-			  (format layer "~[(~]~[_~;S~]~[)~]"
-				  print-parens
-				  (if (= j scanpos) 1 0)
-				  print-parens)))))))))
+;;===================================================================
+
+(defclass fw-state ()
+  ((simtime :initarg :simtime :initform 0 :accessor fw-state-simtime)
+   (layers :initarg :layers :initform nil :accessor fw-state-layers)))
+
+(defmethod print-object ((s fw-state) stream)
+  (with-slots (simtime layers) s
+    (format stream "#<fw-state: simtime ~a" simtime)
+    (dolist (l layers)
+      (format stream "~%  ~a" l))
+    (format stream ">~%")))
+
+(defgeneric prt-state (fwstate pos))
+(defmethod prt-state ((fwstate fw-state) pos)
+  (with-slots (simtime layers) fwstate
+    (format t "Time ~d step ~d:~%" simtime pos)
+    (dotimes (i (1+ *max-depth*))
+      (let ((n (find-layer-with-depth layers i)))
+	(if (null n)
+	    (format t "~3d ~[...~;(X)~]~%" i (if (= i pos) 1 0))
+	    (let ((scanpos (layer-scanpos n))
+		  (r (layer-range n)))
+	      (format t "~3d ~a~%" i
+		      (with-output-to-string (layer)
+			(dotimes (j r)
+			  (format layer "~a"
+				  (cond ((= j 0)
+					 (if (= 0 scanpos)
+					     (if (= i pos) "*" "S")
+					     (if (= i pos) "X" "_")))
+					(t
+					 (if (= j scanpos) "S" "_"))))))))))))
   (format t "~%"))
 
-;; Update the scan positions for each defined depth in this state
-(defun advance-scanners (state)
-  (dolist (s state)
-    (let* ((increment (layer-increment s))
-	   (scanpos (+ (layer-scanpos s) increment)))
-      (cond ((>= scanpos (layer-range s))
-	     (decf scanpos 2)
-	     (setf (layer-increment s) (- increment)))
-	    ((= -1 scanpos)
-	     (setf scanpos 1)
-	     (setf (layer-increment s) (- increment))))
-      (setf (layer-scanpos s) scanpos)))
-  state)
+;; *simtimelist* is a queue containing elements of type fw-state
+(defparameter *simtimelist* (make-instance 'dls:queue))
+
+;; *simtimelist* keeps the last 100 picoseconds of data so we don't have to keep regenerating it
+;; NB: this number was derived from the fact that the default input has ~100 layers.
+(defconstant +max-to-keep+ 100)
+
+(let ((max-kept 0))
+
+  ;; This discards the oldest data, in accordance with +max-to-keep+.
+  (defun prune-simlist ()
+    (do ()
+	((<= max-kept +max-to-keep+))
+      (let ((s (dls:dequeue *simtimelist*)))
+	(if (> *verbose* 2)
+	    (format t "Deleted state at time ~d~%" (fw-state-simtime s)))
+	(free-layer-list (fw-state-layers s))
+	(setf (fw-state-layers s) nil) ;; hopefully avoid some garbage
+	(decf max-kept))))
+
+  ;; The new state is either the original state at time 0, or just
+  ;; the previous state, one tick later with the scanners advanced by one.
+  (defun fw-state-add (simtime prevstate)
+    (when (> simtime 0)
+      (assert (typep prevstate 'fw-state) (prevstate))
+      (assert (not (null (fw-state-layers prevstate))) (prevstate)))
+    (let* ((layerlist (if (= 0 simtime)
+			  (copy-layer-list *input-list*)
+			  (mapcar #'layer-advance (copy-layer-list (fw-state-layers prevstate)))))
+	   (s (make-instance 'fw-state :simtime simtime :layers layerlist)))
+      (if (> *verbose* 2)
+	  (format t "Added state at time ~d~%" simtime))
+      (dls:enqueue *simtimelist* s)
+      (incf max-kept)
+      (prune-simlist)
+      (assert (typep s 'fw-state) (s))
+      (assert (not (null (fw-state-layers s))) (s))
+      (assert (typep (first (fw-state-layers s)) 'layer) (s))
+      s)))
 
 ;; Returns a layer-list from a fw-state
 (defun fw-state-at-time (time)
@@ -228,7 +204,8 @@
 			 (when (> simtime max-seen)
 			   (setf max-seen simtime)
 			   (setf prev n))
-			 (= simtime time))) (members *simtimelist*))))
+			 (= simtime time)))
+		     (dls:members *simtimelist*))))
     ;; (format t "initial search: ~a max-seen: ~a~%" s max-seen)
     (unless s
       (let ((num-missing (- time max-seen)))
@@ -236,46 +213,35 @@
 	    (format t "adding ~a missing states~%" num-missing))
 	(do ((i (1+ max-seen) (1+ i)))
 	    ((> i time))
-	  (when (and (> num-missing 1) (= (mod i 100000) 0))
+	  (when (and (> num-missing 1) (> i 0) (= (mod i 100000) 0))
 	    (format t "~a " i)
 	    (finish-output))
-	  ;; The new state is either the original state at time 0, or just
-	  ;; the previous state, one tick later with the scanners advanced by one.
-	  (setf s (make-instance
-		   'fw-state
-		   :simtime i
-		   :layers (if (= 0 i)
-			       (copy-layer-list *input-list*)
-			       (let ((l (copy-layer-list (fw-state-layers prev))))
-				 (advance-scanners l)))))
-	  (add-fw-state s)
+	  (setf s (fw-state-add i prev))
 	  (setf prev s))
 	(if (> num-missing 1)
 	    (format t "Missing states added~%"))))
-    (fw-state-layers s)))
+    s))
 
-;; state is a list of layers
-;; returns (caught severity)
-(defun check-caught (depth state)
-  (let ((n (find-layer-with-depth state depth)))
-    (cond ((and n (= 0 (layer-scanpos n)))
-	   (if (> *verbose* 0)
-	       (format t "caught at depth ~a range ~a~%" depth (layer-range n)))
-	   (values 1 (* depth (layer-range n))))
-	  (t
-	   (values 0 0)))))
+;; ====================
+
+(defun check-caught (depth layerlist)
+  (let ((n (find-layer-with-depth layerlist depth)))
+    (values (and n (= 0 (layer-scanpos n))) (if n (* depth (layer-range n)) 0))))
 
 (defun do-trip (delay)
-  (let ((severity 0))
+  (let ((severity 0)
+	(ever-caught nil))
     (loop for i from 0 to *max-depth*
        do (let ((state (fw-state-at-time (+ i delay))))
 	    (when (> *verbose* 0)
-	      (format t "Time ~d step ~d:~%" (+ i delay) i)
 	      (prt-state state i))
-	    (multiple-value-bind (caught sev) (check-caught i state)
-	      (if caught
-		  (incf severity sev)))))
-    severity))
+	    (multiple-value-bind (caught sev) (check-caught i (fw-state-layers state))
+	      (when caught
+		(if (> *verbose* 0)
+		    (format t "caught at depth ~a severity ~a~%" i sev))
+		(setf ever-caught t)
+		(incf severity sev)))))
+    (values ever-caught severity)))
 
 (defun main (args)
   (let ((rlow 0)
@@ -301,13 +267,13 @@
 
     (format t "trying range(~a,~a)~%" rlow rhigh)
     (loop for delay from rlow below rhigh
-       do (let ((sev (do-trip delay)))
-	    (when (= 0 sev)
+       do (multiple-value-bind (caught sev) (do-trip delay)
+	    (when (and (not caught) (= 0 sev))
 	      (format t "smallest delay: ~a~%" delay)
 	      (sb-ext:exit :code 0))
-	    (if (> *verbose* 0)
+	    (if (or (> *verbose* 0) (= (1- rhigh) delay))
 		(format t "delay ~a: severity ~a~%" delay sev))
-	    (when (= 0 (mod delay 1000))
+	    (when (= 0 (mod delay 10000))
 	      (format t ".")
 	      (finish-output)))))
   
