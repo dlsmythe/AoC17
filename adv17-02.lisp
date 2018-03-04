f37e19a9035ac18d3e2d2593e4533a133f9a8fea
Dave Smythe <dlsmythe@gmail.com>
2018-03-03 19:42:38 -0600

    Update 2 lisp. Added text.

--- adv17-02.lisp
+++ adv17-02.lisp
@@ -1,3 +1,6 @@
+;;; New here:
+;;; - with-input-from-string
+;;; - process a file a line at a time (without the loop macro)
 
 (defun parse-vals (str)
   (with-input-from-string (s str) (loop for x = (read s nil nil) while x collect x)))
@@ -11,28 +14,27 @@
       (let ((vals (parse-vals line)))
 	(let ((minval most-positive-fixnum)
 	      (maxval most-negative-fixnum)
-	      (result 0)
 	      (numerator 0)
-	      (divisor -1)
+	      (divisor 0)
 	      (numvals (length vals)))
 	  (do* ((i 0 (1+ i))
 		(v (nth i vals) (nth i vals)))
-	       ((or (>= i numvals) (> 0 result)))
+	       ((>= i numvals))
 	    (if (> v maxval)
 		(setf maxval v))
 	    (if (< v minval)
 		(setf minval v))
 	    (do ((j 0 (1+ j)))
-		((or (>= j numvals) (/= -1 divisor)))
-	      (let ((v2 (nth j vals)))
-		(unless (or (= i j) (= 0 v2))
-		  (cond ((= 0 (mod v v2))
+		((or (>= j numvals) (/= 0 divisor)))
+	      (unless (= i j)
+		(let ((v2 (nth j vals)))
+		  (cond ((and  (/= 0 v2) (= 0 (mod v v2)))
 			 (setf numerator i)
 			 (setf divisor j))
 
 			;; NB: This test is unnecessary - it would be performed
 			;;      eventually in any case.  This is a just an optimization.
-			((= 0 (mod v2 v))
+			((and (/= 0 v) (= 0 (mod v2 v)))
 			 (setf numerator j)
 			 (setf divisor i)))))))
 
